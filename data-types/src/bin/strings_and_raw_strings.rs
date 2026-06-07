bed41192ab4bcee680d9127dc62c712bebe8fcce
kkamara <kamaracomputers@gmail.com>
2026-06-07 17:07:22 +0100

    ~

---
+++ data-types/src/bin/strings_and_raw_strings.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Dear Emily,\nHow have you been?");
+    println!("\tOnce upon a time...");
+    println!("Juliet said, \"I love you, Romeo.\"");
+    let mut fp = "C:\\Users\\Emily\\Documents\\file.txt";
+    println!("File path: {}", fp);
+    fp = r"C:\Users\Emily\Documents\file.txt";
+    println!("File path with raw string: {}", fp);
+}
