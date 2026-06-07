f32cc272415b83a38b15d5a2394f3a72e95d38ab
kkamara <kamaracomputers@gmail.com>
2026-06-07 15:37:36 +0100

    Create project: data-types

---
+++ data-types/src/bin/integers.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let sixteen_bit_signed: i16 = -30_000;
+    let sixteen_bit_unsigned: u16 = 65_000;
+
+    let thirty_two_bit_signed: i32 = -2_000_000_000;
+    let thirty_two_bit_unsigned: u32 = 4_000_000_000;
+
+    let some_value = 20u16; // 20 as an 8-bit signed integer
+}
