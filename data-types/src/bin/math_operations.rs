5daf60720ebc75351cdd76e19aa475ebff5cf98c
kkamara <kamaracomputers@gmail.com>
2026-06-08 17:21:14 +0100

    ~

---
+++ data-types/src/bin/math_operations.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let addition = 5 + 4;
+    let subtraction = 10 - 6;
+    let multiplication = 3 * 4;
+    println!("Addition: {}, Subtraction: {}, Multiplication: {}", addition, subtraction, multiplication);
+
+    let floor_division = 5 / 3;
+    println!("Floor division: {floor_division}");
+
+    let decimal_division = 5.0 / 3.0;
+    println!("Decimal division: {decimal_division}");
+
+    let remainder = 7 % 2;
+    println!("Remainder: {remainder}"); // 1
+
+    let remainder = 8 % 2;
+    println!("Remainder: {remainder}"); // 0
+}
