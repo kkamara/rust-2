44c28ff1ad0966588ab8436880be1f79c188bce7
kkamara <kamaracomputers@gmail.com>
2026-06-11 17:07:42 +0100

    ~

---
+++ data-types/src/bin/equality_and_inequality_operators.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{}", "Coke" == "Pepsi");
+    println!("{}", "Coke" != "Pepsi");
+    println!("{}", "Coke" == "Coke");
+    println!("{}", "Coke" == "Coke ");
+
+    println!("{}", 13 == 13);
+    println!("{}", 13 != 13);
+
+    println!("{}", 26.1 == 26.1);
+    println!("{}", 26.1 == 26.14);
+
+    println!("{}", 13 == 13.0 as i32);
+
+    println!("{}", true == true);
+    println!("{}", false == false);
+    println!("{}", true != false);
+}
